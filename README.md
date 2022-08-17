f1e9dba9279ef95566ffd363ef242e695a64eb42
Jason A. Dour <10261648+jason-dour@users.noreply.github.com>
2022-08-16 22:56:05 -0400

    Baseline.
    - Lessons learned from gomplate action applied to this one.
    - Should be functional.  Need to test via Action.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # action-setup-hugo-preproc
+
+![Action Tests](https://github.com/jason-dour/action-setup-hugo-preproc/actions/workflows/test.yml/badge.svg)
+
 Set up your GitHub Actions workflow with a specific version of hugo-preproc.
+
+## Usage
+
+Input `hugo-preproc-version` is optional; default is to install the latest version. See [hugo-preproc releases](https://github.com/jason-dour/hugo-preproc/releases) for list of specific semver release tags.
+
+### Basic
+
+Add a step that calls `jason-dour/action-setup-hugo-preproc`, providing the `GITHUB_TOKEN` from the workflow as an environment variable.  **This is required**.
+
+```yaml
+steps:
+- uses: actions/checkout@v3
+- uses: jason-dour/action-setup-hugo-preproc@v1.0.0
+  env:
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+- run: hugo-preproc --version
+```
+
+### Specific hugo-preproc Version
+
+Optionally, you may also specify the semver release tag of a specific hugo-preproc release to be installed.
+
+```yaml
+steps:
+- uses: actions/checkout@v3
+- uses: jason-dour/action-setup-hugo-preproc@v1.0.0
+  with:
+    hugo-preproc-version: v3.10.0
+  env:
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+- run: hugo-preproc --version
+```
---
+++ index.js
@@ -0,0 +1,87 @@
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+
+const core = require("@actions/core");
+const gh = require("@actions/github");
+const tc = require("@actions/tool-cache");
+
+// Grab the OS and Arch.
+const myPlat = os.platform();
+const myArch = os.arch();
+
+// Leverage the GitHub Action environment variables to authenticate with GitHub
+const octokit = new gh.getOctokit(process.env.GITHUB_TOKEN);
+
+// getRelease returns the octokit release object for the given version
+async function getRelease(version) {
+  var release;
+  try {
+    if (version === "latest") {
+      release = await octokit.rest.repos.getLatestRelease({
+        owner: "jason-dour",
+        repo: "hugo-preproc",
+      });
+    } else {
+        release = await octokit.rest.repos.getReleaseByTag({
+          owner: "jason-dour",
+          repo: "hugo-preproc",
+          tag: version,
+        });
+    }
+  } catch (e) {
+    core.setFailed(e);
+  }
+  return release
+}
+
+// getDownloadObject returns an object with the following properties:
+//   url: the url to download the tool from
+async function getDownloadObject(version) {
+  const release = await getRelease(version);
+  core.debug("release: " + release.data.name);
+  const asset = release.data.assets.find((asset) =>
+    asset.name.includes(
+      `_${myPlat}_${myArch}`
+    )
+  );
+  core.debug("asset: " + asset)
+  const url = asset.browser_download_url;
+  core.info("url: " + url);
+  return { url };
+}
+
+// setup downloads the tool and installs it to the given path
+async function setup() {
+  try {
+    // Get version of tool to be installed
+    // const version = core.getInput("hugo-preproc-version");
+    const version = "latest"
+    core.debug("version: " + version);
+
+    // Download the specific version of the tool.
+    const download = await getDownloadObject(version);
+
+    var exeName = "";
+    if (myPlat === "win32") {
+      exeName = path.join(process.env.RUNNER_TEMP,"hugo-preproc.exe");
+    } else {
+      exeName = path.join(process.env.RUNNER_TEMP,"hugo-preproc.");
+    }
+    const pathToCLI = await tc.downloadTool(
+      download.url,
+      exeName
+    );
+    fs.chmodSync(pathToCLI, 0o755); // make the binary executable
+    core.debug("pathToCLI: " + pathToCLI);
+
+    // Expose the tool by adding it to the PATH
+    core.addPath(process.env.RUNNER_TEMP);
+  } catch (e) {
+    core.setFailed(e);
+  }
+}
+
+export default setup;
+
+setup();
